1f477711593d9b7255511c0760be524b13ed8787
Sergei <radchenkovsa@yandex.ru>
2023-09-03 22:23:04 +0300

    Case4

--- case4/Program.cs
+++ case4/Program.cs
@@ -4,3 +4,28 @@
 Если N = 3 -> 0 1 1
 Если N = 7 -> 0 1 1 2 3 5 8 */
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void RowFibo(int members)
+{
+    int first = 0;
+    int second = 1;
+    if (members == 1)
+        Console.Write(first);
+    else    
+        Console.Write($"{first} {second}");
+        for (int i = 3; i <= members; i++)
+        {
+            int next = first + second;
+            Console.Write($" {next}");
+            first = second;
+            second = next;
+        }
+}
+
+int number = InputNum("Введите количество чисел в ряду: ");
+RowFibo(number);

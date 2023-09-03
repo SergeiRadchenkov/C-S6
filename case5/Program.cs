15a80bf1f5f6373b4b4f1093863fd70d6ae5470f
Sergei <radchenkovsa@yandex.ru>
2023-09-03 22:37:01 +0300

    Case5

--- case5/Program.cs
+++ case5/Program.cs
@@ -1,3 +1,45 @@
 ﻿/* Напишите программу, которая будет создавать копию
 заданного массива с помощью поэлементного копирования. */
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] arr, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] =rnd.Next(minValue, maxValue + 1);
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + " ");
+    Console.WriteLine();
+}
+
+int[] ArrayCopy(int[] arr)
+{
+    int[] res = CreateArray(arr.Length);
+
+    for (int i = 0; i < arr.Length; i++)
+       res[i] = arr[i];
+    return res;
+}
+
+int size = InputNum("Введите размер массива: ");
+int[] array = CreateArray(size);
+int min = InputNum("Введите минимальное значение элемента: ");
+int max = InputNum("Введите максимальное значение элемента: ");
+FillArray(array, min, max);
+PrintArray(array);
+int[] arrCopy = ArrayCopy(array);
+PrintArray(arrCopy);

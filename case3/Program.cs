3172f0641453bd402054ffcfa3ab0ac8e0382397
Sergei <radchenkovsa@yandex.ru>
2023-09-03 21:47:32 +0300

    Case3

--- case3/Program.cs
+++ case3/Program.cs
@@ -4,3 +4,24 @@
 3 -> 11
 2 -> 10 */
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+string DecToBin(int decNum)
+{
+    string bin = "";
+    while (decNum != 0)
+    {
+        int reminder = decNum % 2;
+        bin = $"{reminder}" + bin;
+        decNum = decNum / 2;
+    }
+    return bin;
+}
+
+int number = InputNum("Введите число: ");
+string answer = DecToBin(number);
+Console.WriteLine(answer);

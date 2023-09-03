0a05ce7800e7335874a280ff6c3b8565a191c0ca
Sergei <radchenkovsa@yandex.ru>
2023-09-03 19:11:48 +0300

    Семинар №6

--- case1/Program.cs
+++ case1/Program.cs
@@ -1,6 +1,23 @@
-﻿/* Напишите программу, которая перевернёт одномерный
+/* Напишите программу, которая перевернёт одномерный
 массив (последний элемент будет на первом месте, а первый - на
 последнем и т.д.)
 [1 2 3 4 5] -> [5 4 3 2 1]
 [6 7 3 6] -> [6 3 7 6 */
 
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+    return new int[size];
+
+void FillArray(int[] arr, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] =rnd.Next(minValue, maxValue + 1);
+}
+
+void PrintArray
